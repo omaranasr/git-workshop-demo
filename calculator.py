e4b288131cf952c06eab9d3c3fdc1d62c218b0f4
omaranasr <omaranasr@cu.edu.eg>
2026-03-05 21:59:01 +0200

    Add multiply function to calculator python file

--- calculator.py
+++ calculator.py
@@ -8,6 +8,12 @@ def subtract(a, b):
     """Return the difference of a and b."""
     return a - b
 
+def multiply(a, b):
+    """Return the product of a and b."""
+    return a * b
+
+
 if __name__ == "__main__":
     print("Addition:    ", add(10, 5))
     print("Subtraction: ", subtract(10, 5))
+    print("Multiply:    ", multiply(10, 5))
