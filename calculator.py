a6a9bc6a837a4abaa2c17f3e6910ccd37d259be3
omaranasr <omaranasr@cu.edu.eg>
2026-03-05 21:49:19 +0200

    Add simple calculator functions for addition and subtraction in a python file

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# calculator.py - Simple calculator for Git workshop
+
+def add(a, b):
+    """Return the sum of a and b."""
+    return a + b
+
+def subtract(a, b):
+    """Return the difference of a and b."""
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition:    ", add(10, 5))
+    print("Subtraction: ", subtract(10, 5))
