7e5d35e67898d957d5d6200dab0ef3f03d6988f1
omaranasr <omaranasr@cu.edu.eg>
2026-03-05 22:24:12 +0200

    added divide example in manin

--- calculator.py
+++ calculator.py
@@ -24,3 +24,4 @@ if __name__ == "__main__":
     print("Addition:    ", add(10, 5))
     print("Subtraction: ", subtract(10, 5))
     print("Multiply:    ", multiply(10, 5))
+    print("Division:    ", divide(10, 5))
